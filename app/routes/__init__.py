9c5f0bc783c179bc9ee2e3e2f8ce4338ff595bf1
lperez99 <laura.perez1999@hotmail.com>
2025-08-08 12:36:42 -0500

    upload __init__ file

--- app/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Create a blueprint for the routes
-api = Blueprint('api', __name__)
-
-# Import routes
-from .loan_routes import *
-from .purchase_routes import *
-from .inventory_routes import *
