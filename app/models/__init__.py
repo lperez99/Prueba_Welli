43a9cecb22d09ef50e92a3b0b44ad44decbfaee6
lperez99 <laura.perez1999@hotmail.com>
2025-08-08 12:33:54 -0500

    update file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,3 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    user_type = Column(String, nullable=False)
+from .user import User
+from .book import Book
+from .loan import Loan
