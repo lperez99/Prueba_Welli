4326523778d725cd98bc5e74ec59d66e07237fc0
lperez99 <laura.perez1999@hotmail.com>
2025-08-08 20:39:08 -0500

    change importation to the top

--- app/routes/loan.py
+++ app/routes/loan.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schema.loan import LoanCreate, LoanOut
-from app.crud import loan as loan_crud
+from app.services import loan
 from app.database import SessionLocal
+from app.services import reservation 
 
 router = APIRouter()
 
@@ -16,25 +17,25 @@ def get_db():
 @router.post("/", response_model=LoanOut)
 def create_loan_endpoint(loan_in: LoanCreate, db: Session = Depends(get_db)):
     try:
-        return loan_crud.create_loan(db, loan_in)
+        return loan.create_loan(db, loan_in)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.put("/{loan_id}/extend", response_model=LoanOut)
 def extend_loan_endpoint(loan_id: int, db: Session = Depends(get_db)):
     try:
-        return loan_crud.extend_loan(db, loan_id)
+        return loan.extend_loan(db, loan_id)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     
 @router.get("/", response_model=list[LoanOut])
 def list_loans(db: Session = Depends(get_db)):
-    return loan_crud.get_loans(db)
+    return loan.get_loans(db)
 
 @router.put("/{loan_id}/return", response_model=LoanOut)
 def return_loan_endpoint(loan_id: int, db: Session = Depends(get_db)):
     try:
-        return loan_crud.return_loan(db, loan_id)
+        return loan.return_loan(db, loan_id)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
